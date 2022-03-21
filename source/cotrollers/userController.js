f65e468207e186333becb2ca5c3ca52a3ca5c513
aeolian83 <aeolian83@gmail.com>
2022-03-22 07:42:28 +0900

    #4.6
    Planning Routers!!!

--- source/cotrollers/userController.js
+++ source/cotrollers/userController.js
@@ -2,3 +2,6 @@
 export const join = (req, res) => res.send("Welcome Join!!");
 export const edit = (req, tes) => res.send("Edit User!");
 export const remove = (req, res) => res.send("Remove User!!");
+export const login = (req, res) => res.send("login");
+export const logout = (req, res) => res.send("logout");
+export const see = (req, res) => res.send("See User");
--- source/routers/globalRouter.js
+++ source/routers/globalRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { trending } from "../cotrollers/videoController";
-import { join } from "../cotrollers/userController";
+import { trending, search } from "../cotrollers/videoController";
+import { join, login } from "../cotrollers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
 
