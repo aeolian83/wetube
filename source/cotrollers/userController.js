7f22a49fb5b009fdd49ca345782d80d72e067390
aeolian83 <aeolian83@gmail.com>
2022-03-25 08:42:22 +0900

    #5.8
    Iteration!!
    pug, ul, li, variables

--- source/cotrollers/userController.js
+++ source/cotrollers/userController.js
@@ -1,4 +1,3 @@
-
 export const join = (req, res) => res.send("Welcome Join!!");
 export const edit = (req, tes) => res.send("Edit User!");
 export const remove = (req, res) => res.send("Remove User!!");
--- source/cotrollers/videoController.js
+++ source/cotrollers/videoController.js
@@ -1,14 +1,12 @@
-const fakeUser = {
-    username: "Nicolas",
-    loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-    console.log(req.params);
-    return res.send("Delete Video");
+  console.log(req.params);
+  return res.send("Delete Video");
 };
